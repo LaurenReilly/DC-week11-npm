42addfbc7d99c462379702ab8ad84850cfeae51e
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-12-05 16:45:01 -0500

    completed all exercises

--- index.js
+++ index.js
@@ -1,107 +1,119 @@
 // //LODASH EXERCISES
 // // Load the full build.
-// const _ = require('lodash');
-// var collection = [1, 2, 3, 4, 5];
+const _ = require('lodash');
+var collection = [1, 2, 3, 4, 5];
 
 // //exercise1
-// console.log(_.shuffle(collection));
+console.log(_.shuffle(collection));
 
 // //exercise2
-// console.log(_.mean(collection));
+console.log(_.mean(collection));
 
 // //exercise3
-// console.log(_.isBoolean(collection));
+console.log(_.isBoolean(collection));
 
 // //ROBOTJS EXERCISES
-// var robot = require("robotjs");
+var robot = require("robotjs");
 
-// robot.typeString("I AM A FELLOW HUMAN AND NOT A ROBOT");
+robot.typeString("I AM A FELLOW HUMAN AND NOT A ROBOT");
 
-// robot.moveMouse(0, 400);
-// robot.moveMouseSmooth(1800, 400);
+robot.moveMouse(0, 400);
+robot.moveMouseSmooth(1800, 400);
 
 
 // WALLPAPER AXIOS AND DOWNLOAD FILE
-// const wallpaper = require('wallpaper');
-// var download = require('download-file');
-// var axios = require('axios');
+const wallpaper = require('wallpaper');
+var download = require('download-file');
+var axios = require('axios');
 
-// var options = {
-//     directory: "./",
-//     filename: ""
-// }
+var options = {
+    directory: "./",
+    filename: ""
+}
 
-// axios.get("https://dog.ceo/api/breeds/image/random").then(function(data) {
-//     var image = data.data.message;
-//     download(image, options, function(err){
-//         if (err) throw err
-//         wallpaper.set(options.filename);
-//     });
-// });
+axios.get("https://dog.ceo/api/breeds/image/random").then(function(data) {
+    var image = data.data.message;
+    download(image, options, function(err){
+        if (err) throw err
+        wallpaper.set(options.filename);
+    });
+});
 
 
 
 //CATFACTS PDF
-// const catFacts = require('cat-facts');
-// let randomFact = catFacts.random();
+const catFacts = require('cat-facts');
+let randomFact = catFacts.random();
 
-// //initialize variables
-// var PDFDocument, doc;
-// var fs = require('fs');
+//initialize variables
+var PDFDocument, doc;
+var fs = require('fs');
 
-// PDFDocument = require('pdfkit');
-// //create new instance of PDFDocument class
-// doc = new PDFDocument;
-// //create document
-// doc.pipe(fs.createWriteStream('output.pdf'));
-// doc.text(randomFact);
-// doc.end();
+PDFDocument = require('pdfkit');
+//create new instance of PDFDocument class
+doc = new PDFDocument;
+//create document
+doc.pipe(fs.createWriteStream('output.pdf'));
+doc.text(randomFact);
+doc.end();
 
 //PRETTIER JAVASCRIPT
-const prettier = require('prettier');
+//in package.json scripts
 
-//I dont even...what?
+
 
 //NODEMAILER EXERCISE
-'use strict';
 const nodemailer = require('nodemailer');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
+// Generate SMTP service account from ethereal.email
 nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
+    if (err) {
+        console.error('Failed to create a testing account. ' + err.message);
+        return process.exit(1);
+    }
+
+    console.log('Credentials obtained, sending message...');
+
+    // Create a SMTP transporter object
     let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
         auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
+            user: account.user,
+            pass: account.pass
         }
     });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: 'bar@example.com, baz@example.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
+    // Message object
+    let message = {
+        from: 'Lauren <sender@example.com>',
+        to: 'Lauren <recipient@example.com>',
+        subject: 'Nodemailer is unicorn friendly ✔',
+        text: 'Hey girl hey!',
+        html: '<p><b>YO</b> Harry Potter!</p>'
     };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Error occurred. ' + err.message);
+            return process.exit(1);
         }
+
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
 });
 
+
+
+
+// nerdJS (extra npm module)
+
+let nerds = require('nerds');
+
+let singleHP = nerds.resolve('Harry Potter').asArray();
+console.log(singleHP);
 
     
